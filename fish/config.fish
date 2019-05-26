0be777eba7c327f9cb5eab2f28621a9a7ceea653
Robert Schäfer <git@roschaefer.de>
2019-05-27 00:51:22 +0200

    Update as of 2019-05-27

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,3 @@
-# If running trom tty1 start sway
-switch (tty)
-case /dev/tty1
-    export XKB_DEFAULT_OPTIONS=ctrl:nocaps,lv3:ralt_switch_multikey
-    sway
-    exit 0
-end
-
 export EDITOR=nvim
 
 
@@ -16,10 +8,30 @@ alias gc="git commit"
 alias gd="git diff"
 alias gdca="git diff --cached"
 
-source /usr/share/chruby/chruby.fish
 
-export PATH="$HOME/.nodenv/shims:$PATH"
+# javascript
 export PATH="$HOME/.yarn/bin:$PATH"
+# export PATH="$HOME/.nodenv/shims:$PATH"
 
-export ANDROID_HOME="/opt/android-sdk/"
+# android
+export ANDROID_HOME="/opt/android-sdk"
 export PATH="$ANDROID_HOME/platform-tools:$PATH"
+
+# flutter
+export PATH="/opt/flutter/bin:$PATH"
+
+# neo4j
+export PATH="/usr/share/neo4j/bin/:$PATH"
+
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+# kubeconfig files
+export KUBECONFIG="$HOME/.kube/config:$HOME/.kube/nitro-staging-kubeconfig.yaml:$HOME/.kube/nitro-production-kubeconfig.yaml"
+
+if status is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+        exec startx -- -keeptty
+    end
+end
+source ~/.asdf/asdf.fish
