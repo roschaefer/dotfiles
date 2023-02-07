e87508e22621f8c73f5435cb86c438715f8611f3
Robert Schäfer <git@roschaefer.de>
2023-02-07 11:18:58 +0100

    chore: rewamp dotfiles on new machine

--- fish/config.fish
+++ fish/config.fish
@@ -4,24 +4,23 @@ export BROWSER=firefox
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export CHROME_EXECUTABLE=(which chromium)
 export BACKUP_DIR=/run/media/robert/external-drive/home/robert
+export GPG_TTY=$(tty)
 
-alias fm="ranger"
+alias fm="vifm"
 alias gst="git status"
 alias ga="git add"
 alias gc="git commit"
 alias gd="git diff"
 alias gdca="git diff --staged"
-
 alias backup_local_drive="rsync -avP --stats --exclude-from=/home/robert/.config/rsync/rsync-homedir-local.txt /home/robert/ $BACKUP_DIR"
 
-source /opt/asdf-vm/asdf.fish
-
-function mark_prompt_start --on-event fish_prompt
-    echo -en "\e]133;A\e\\"
-end
-
-set TTY1 (tty)
-if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
-  set -x (gnome-keyring-daemon --start | string split "=")
+if status is-login
+  set -x (gnome-keyring-daemon | string split "=")
   exec sway
 end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+source /opt/asdf-vm/asdf.fish
---
+++ nvim/init.lua
@@ -0,0 +1,177 @@
+require 'plugins'
+
+vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappings are correct
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup('plugins', {})
+
+vim.o.clipboard = 'unnamedplus'
+vim.wo.number = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+
+vim.keymap.set('n', '<leader>t', '<cmd>tabnew<cr>')
+vim.keymap.set('n', 'H', '<cmd>tabprevious<cr>')
+vim.keymap.set('n', 'L', '<cmd>tabnext<cr>')
+vim.keymap.set('c', 'W', 'w')
+
+vim.cmd('colorscheme gruvbox')
+
+require'lspconfig'.tsserver.setup {}
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+
+
+-- Mappings.
+-- See `:help vim.lsp.*` for documentation on any of the below functions
+local bufopts = { noremap=true, silent=true, buffer=bufnr }
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+vim.keymap.set('n', '<leader>wl', function()
+  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+end, bufopts)
+vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+vim.keymap.set('n', '<leader>fo', function() vim.lsp.buf.format { async = true } end, bufopts)
+
+require('lspconfig')['tsserver'].setup{
+  on_attach = on_attach,
+  flags = lsp_flags,
+}
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fH', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fh', builtin.oldfiles, {})
+
+require("telescope").load_extension "file_browser"
+vim.keymap.set(
+"n",
+"<leader>fe",
+"<cmd>lua require 'telescope'.extensions.file_browser.file_browser()<CR>",
+{noremap = true}
+)
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 40,
+    mappings = {
+      list = {
+        { key = "l", action = "dir_down" },
+        { key = "h", action = "close_node" },
+      },
+    },
+  },
+})
+vim.keymap.set('n', '<leader>e', '<Cmd>NvimTreeFindFileToggle<cr>')
+
+local cmp = require'cmp'
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    -- { name = 'vsnip' }, -- For vsnip users.
+    { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require('lspconfig')['tsserver'].setup {
+  capabilities = capabilities
+}
+
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+require('leap').add_default_mappings()
+
+require"octo".setup()
