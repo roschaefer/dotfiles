68451916ad4030ac108005794e8caa202a065a6f
Robert Schäfer <git@roschaefer.de>
2024-07-05 15:28:40 +0200

    fix: editor lsp settings
    Globally installed gems like `solargraph` should get precedence over
    gems installed via `asdf`

--- fish/config.fish
+++ fish/config.fish
@@ -6,8 +6,6 @@ export BACKUP_DIR=/run/media/robert/external-drive/home/robert
 export GPG_TTY=$(tty)
 export MOZ_ENABLE_WAYLAND=1
 
-source /opt/asdf-vm/asdf.fish
-
 function lfcd
     set tmp (mktemp)
     # `command` is needed in case `lfcd` is aliased to `lf`
@@ -23,33 +21,19 @@ function lfcd
     end
 end
 
-# pnpm
-set -gx PNPM_HOME "/home/robert/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
-alias gl="git pull"
-alias gf="git fetch"
-alias gst="git status"
-alias ga="git add"
-alias gc="git commit"
-alias gd="git diff"
-alias gdca="git diff --staged"
-alias lf="lfcd"
-alias p="pnpm"
-alias backup_local_drive="rsync -avP --stats --exclude-from=/home/robert/.config/rsync/rsync-homedir-local.txt --exclude=node_modules --exclude=.yarn/unplugged /home/robert/ $BACKUP_DIR"
-
 if status is-login
   set -x (gnome-keyring-daemon | string split "=")
   exec sway
 end
 
 if status is-interactive
-  # direnv hook fish | source
   zoxide init fish | source
 end
 
-
+source /opt/asdf-vm/asdf.fish
 fish_add_path ~/.cargo/bin
+fish_add_path "$(gem env user_gemhome)/bin"
+
+alias gst="git status"
+alias lf="lfcd"
+alias backup_local_drive="rsync -avP --stats --exclude-from=/home/robert/.config/rsync/rsync-homedir-local.txt --exclude=node_modules --exclude=.yarn/unplugged /home/robert/ $BACKUP_DIR"
