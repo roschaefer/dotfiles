d17960b9337210cf9ecc195802cdb37cebb0d372
Robert Schäfer <git@roschaefer.de>
2024-08-07 16:46:46 +0200

    refactor: replace lf with yazi

--- fish/config.fish
+++ fish/config.fish
@@ -6,19 +6,14 @@ export BACKUP_DIR=/run/media/robert/external-drive/home/robert
 export GPG_TTY=$(tty)
 export MOZ_ENABLE_WAYLAND=1
 export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gcr/ssh
-function lfcd
-    set tmp (mktemp)
-    # `command` is needed in case `lfcd` is aliased to `lf`
-    command lf -last-dir-path=$tmp $argv
-    if test -f "$tmp"
-        set dir (cat $tmp)
-        rm -f $tmp
-        if test -d "$dir"
-            if test "$dir" != (pwd)
-                cd $dir
-            end
-        end
-    end
+
+function yy
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
 end
 
 if status is-login
@@ -29,10 +24,15 @@ if status is-interactive
   zoxide init fish | source
 end
 
+alias gst="git status"
+alias gd="git diff"
+alias gds="git diff --staged"
+alias gs="git stage"
+alias gc="git commit"
+alias dr="sudo systemctl restart docker.service"
+alias backup_local_drive="rsync -avP --stats --exclude-from=/home/robert/.config/rsync/rsync-homedir-local.txt --exclude=node_modules --exclude=.yarn/unplugged /home/robert/ $BACKUP_DIR"
+alias createkh='set tmp_script (mktemp); curl -sSL -o "{tmp_script}" https://raw.githubusercontent.com/kube-hetzner/terraform-hcloud-kube-hetzner/master/scripts/create.sh; chmod +x "{tmp_script}"; bash "{tmp_script}"; rm "{tmp_script}"'
+
 source /opt/asdf-vm/asdf.fish
 fish_add_path ~/.cargo/bin
 fish_add_path "$(gem env user_gemhome)/bin"
-
-alias gst="git status"
-alias lf="lfcd"
-alias backup_local_drive="rsync -avP --stats --exclude-from=/home/robert/.config/rsync/rsync-homedir-local.txt --exclude=node_modules --exclude=.yarn/unplugged /home/robert/ $BACKUP_DIR"
